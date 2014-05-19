ecdef8546d1aa5a7ab6a2a3c4487458dfa3c6415
G33k <delarueguillaume@gmail.com>
2014-05-19 16:00:39 +0200

    Version 0.1.0

---
+++ README.md
@@ -0,0 +1,50 @@
+GULP inline-base64
+==================
+
+This helper will inject images and fonts into your css files.
+
+Warning ! This techniques is really efficient with small files (<14 Kb) cause it avoids DNS requests and makes the page loading faster. But for larger files it will be a mistake to use it !
+
+Install it
+----------
+
+```
+npm install --save-dev gulp-inline-base64
+```
+
+Use it
+------
+
+Here is my sass config :
+
+```
+
+var sass = require('gulp-sass'),
+	inline_base64 = require('gulp-inline-base64'),
+	autoprefixer = require('gulp-autoprefixer');
+...
+
+
+// SASS
+gulp.task('sass', function() {
+    return gulp.src([
+        path_src + '/css/**/*.scss',
+        '!' + path_src + '/css/**/_*.scss'
+    ])
+        .pipe(sass({
+            includePaths: [
+                path_src + '/css/',
+                'bower_components/',
+            ],
+            imagePath: path_src
+        }))
+        .pipe(inline_base64({
+            baseDir: path_src,
+            maxSize: 16 * 1024
+        }))
+        .pipe(autoprefixer("last 2 version", "> 1%", {
+            cascade: true
+        }))
+        .pipe(gulp.dest(path_tmp + '/css'))
+});
+```
---
+++ index.js
@@ -0,0 +1,64 @@
+var es = require('event-stream');
+var path = require('path');
+var fs = require('fs');
+var gutil = require('gulp-util');
+var mime = require('mime');
+
+module.exports = function(opts) {
+
+    opts = opts ||  {};
+
+    if (!opts.baseDir) opts.baseDir = path.dirname(module.parent.filename);
+
+    var datauri = function(file, callback) {
+        var app_path = opts.baseDir;
+        var reg_exp = /url\([|\ |\'|\"](.*?)[|\ |\'|\"](|\,(.*?))\)/g;
+        var isStream = file.contents && typeof file.contents.on === 'function' && typeof file.contents.pipe === 'function';
+        var isBuffer = file.contents instanceof Buffer;
+        if (isBuffer) {
+            var str = String(file.contents);
+
+            var matches = [],
+                found,
+                force;
+            while (found = reg_exp.exec(str)) {
+                matches.push({
+                    'txt': found[0],
+                    'url': found[1],
+                    'force': found[2].replace(/(\,|\ )/g, '') == 'true' ? true : false
+                });
+            }
+
+            for (var i = 0, len = matches.length; i < len; i++) {
+                if (matches[i].url.indexOf('data:image') === -1) { //if find -> image already decoded
+                    var filepath = app_path + path.normalize(matches[i].url);
+                    if (fs.existsSync(filepath)) {
+                        var size = fs.statSync(filepath).size;
+
+                        // File will not be included because of his size
+                        if (opts.maxSize && size > opts.maxSize && !matches[i].force) {
+                            gutil.log('gulp-inline-base64:', gutil.colors.yellow('file is greater than ' + opts.maxSize + ', so we skip') + gutil.colors.gray(' (' + filepath + ')'));
+                            str = str.replace(matches[i].txt, 'url(' + matches[i].url + ')');
+                        }
+
+                        // Else replace by inline base64 version
+                        else {
+                            var b = fs.readFileSync(filepath);
+                            str = str.replace(matches[i].txt, 'url(' + ('data:' + mime.lookup(filepath) + ';base64,' + b.toString('base64')) + ')');
+                        }
+
+                    } else {
+                        gutil.log('gulp-inline-base64:', gutil.colors.yellow('file not found and it is passed') + gutil.colors.gray(' (' + filepath + ')'));
+                    }
+                }
+            }
+            file.contents = new Buffer(str);
+
+            return callback(null, file);
+        }
+
+        callback(null, file);
+    };
+
+    return es.map(datauri);
+};
